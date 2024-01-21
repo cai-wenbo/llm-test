35624c6b22aa5ecb77915f06c1e2033667c3b87a
cai-wenbo <508819545@qq.com>
2024-01-21 16:35:20 +0000

    mmlu

---
+++ mmlu.py
@@ -0,0 +1,48 @@
+import torch
+from torch.utils import data
+import transformers
+from transformers import LlamaForCausalLM, LlamaTokenizer, AutoModelForCausalLM, AutoTokenizer
+from transformers import GenerationConfig
+import pandas as pd
+import numpy as np
+import os
+import csv
+
+
+def load_model(model_dir):
+    model = LlamaForCausalLM.from_pretrained(model_dir,
+                                             device_map="balanced",
+                                             torch_dtype=torch.float16
+                                             ).eval()
+    tokenizer = LlamaTokenizer.from_pretrained(model_dir)
+    return model, tokenizer
+
+
+def get_ans(prompt, tokenizer, model):
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu") 
+    prompt_tokens = tokenizer(prompt, return_tensors="pt")
+    logits = model(prompt_tokens.input_ids.to(device), attention_mask = prompt_tokens.attention_mask.to(device)).logits[0,-1]
+
+    print(logits.shape)
+
+
+class PromptLizer():
+    def __init__(self, template):
+        self.template = template
+
+    def __call__(self, record):
+
+
+
+#  def load_dataset(
+
+
+if __name__ == "__main__":
+    model_dir = "./models/llama-2-7b/model"
+    model, tokenizer = load_model(model_dir)
+
+    data = "./dataset"
+    data_path = os.path.join(data_dir, "dev", subject + "_dev.csv")
+    df =  pd.read_csv(data_path)
+    print(df.shape)
+    print(df.head())
